f4cf8ad0e6e8afede1125a9014fcbb9791e354df
geortu <jorge.arturo.biazzo@gmail.com>
2022-09-06 12:10:58 -0300

    front end

--- PortFolioARP/src/app/app.module.ts
+++ PortFolioARP/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraComponent } from './componentes/barra/barra.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraComponent,
+    EncabezadoComponent,
+    AcercaDeComponent
   ],
   imports: [
     BrowserModule,
